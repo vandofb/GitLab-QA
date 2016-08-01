77c24a2e27d67b703137c3e7546f9ab22bd8e402
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-08-01 11:32:43 +0200

    Use page scoped steps to login using credentials

--- spec/main_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-feature 'main page' do
-  scenario 'user visits main page', only: :ce do
-    visit '/'
-
-    expect(page).to have_content('GitLab Community Edition')
-  end
-end
