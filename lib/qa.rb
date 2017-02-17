0f3f32925684402baa0bad7461e1b4068fb99bc5
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-02-17 10:41:48 +0100

    Remove legacy files after extracting instance specs

--- lib/qa.rb
+++ lib/qa.rb
@@ -1,11 +1,6 @@
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 
 module QA
-  module Test
-    autoload :User, 'qa/test/user'
-    autoload :Namespace, 'qa/test/namespace'
-  end
-
   module Scenario
     autoload :Actable, 'qa/scenario/actable'
     autoload :Template, 'qa/scenario/template'
@@ -27,48 +22,10 @@ module QA
         autoload :Upgrade, 'qa/scenario/test/omnibus/upgrade'
       end
     end
-
-    module Gitlab
-      module Project
-        autoload :Create, 'qa/scenario/gitlab/project/create'
-      end
-
-      module License
-        autoload :Add, 'qa/scenario/gitlab/license/add'
-      end
-    end
   end
 
   module Spec
-    autoload :Base, 'qa/spec/base'
-    autoload :Config, 'qa/spec/config'
-    autoload :Run, 'qa/spec/run'
     autoload :Image, 'qa/spec/image'
-  end
-
-  module Page
-    autoload :Base, 'qa/page/base'
-
-    module Main
-      autoload :Entry, 'qa/page/main/entry'
-      autoload :Menu, 'qa/page/main/menu'
-      autoload :Groups, 'qa/page/main/groups'
-      autoload :Projects, 'qa/page/main/projects'
-    end
-
-    module Project
-      autoload :New, 'qa/page/project/new'
-      autoload :Show, 'qa/page/project/show'
-    end
-
-    module Admin
-      autoload :Menu, 'qa/page/admin/menu'
-      autoload :License, 'qa/page/admin/license'
-    end
-  end
-
-  module Git
-    autoload :Repository, 'qa/git/repository'
   end
 
   module Docker
