a4e96bb7f53d52376324e645e473b3c14e40f017
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-03-15 08:12:40 +0100

    Use external Docker Hub as a source of QA images

--- lib/qa/docker/specs.rb
+++ lib/qa/docker/specs.rb
@@ -2,20 +2,17 @@ module QA
   module Docker
     class Specs
       include Scenario::Actable
-      ##
-      # This should be changed to `gitlab/gitlab-qa` after we start
-      # pushing QA images to Docker Hub
-      #
-      IMAGE_NAME = 'registry.gitlab.com/gitlab-org/gitlab-qa'.freeze
+
+      IMAGE_NAME = 'gitlab/gitlab-qa'.freeze
+
+      attr_accessor :env
 
       def initialize
         @docker = Docker::Engine.new
       end
 
-      def env(name)
-        @env = name
-      end
-
+      # rubocop:disable Metrics/AbcSize
+      #
       def test(gitlab)
         tag = "#{gitlab.release}-#{gitlab.tag}"
         args = ['Test::Instance', gitlab.address]
@@ -25,7 +22,7 @@ module QA
 
         @docker.run(IMAGE_NAME, tag, *args) do |command|
           command << "-t --rm --net #{gitlab.network}"
-          command << %Q{-e #{@env}="$#{@env}"} if @env
+          command << %(-e #{env}="$#{env}") if env
           command << "--name #{gitlab.name}-specs"
         end
       end
