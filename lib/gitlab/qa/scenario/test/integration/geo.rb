d81a9e114a56d791d0cefb9a2168ff78801561b4
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-11-13 10:06:06 +0100

    WIP Geo secondary start-up debuggin mode

--- lib/gitlab/qa/scenario/test/integration/geo.rb
+++ lib/gitlab/qa/scenario/test/integration/geo.rb
@@ -11,33 +11,46 @@ module Gitlab
                 raise ArgumentError, 'Geo is EE only!'
               end
 
-              Component::Gitlab.perform do |primary|
-                primary.release = release
-                primary.network = 'geo'
-                primary.omnibus_config = <<~OMNIBUS
-                  geo_primary_role['enable'] = true;
-                  postgresql['listen_address'] = '0.0.0.0';
-                  postgresql['trust_auth_cidr_addresses'] = ['0.0.0.0/0','0.0.0.0/0'];
-                  postgresql['md5_auth_cidr_addresses'] = ['0.0.0.0/0'];
-                  postgresql['max_replication_slots'] = 1;
+              Component::Gitlab.perform do |secondary|
+                secondary.release = release
+                secondary.network = 'geo'
+                secondary.omnibus_config = <<~OMNIBUS
+                  geo_secondary_role['enable'] = true;
                   gitlab_rails['db_key_base'] = '4dd58204865eb41bca93bd38131d51cc';
                 OMNIBUS
 
-                primary.instance do
-                  Component::Gitlab.perform do |secondary|
-                    secondary.release = release
-                    secondary.network = 'geo'
-                    secondary.omnibus_config = <<~OMNIBUS
-                      geo_secondary_role['enable'] = true;
-                      gitlab_rails['db_key_base'] = '4dd58204865eb41bca93bd38131d51cc';
-                    OMNIBUS
-
-                    secondary.instance do
-                      # shellout to instance specs
-                    end
-                  end
+                secondary.instance do
+                  puts 'Secondary started!'
                 end
               end
+
+              # Component::Gitlab.perform do |primary|
+              #   primary.release = release
+              #   primary.network = 'geo'
+              #   primary.omnibus_config = <<~OMNIBUS
+              #     geo_primary_role['enable'] = true;
+              #     postgresql['listen_address'] = '0.0.0.0';
+              #     postgresql['trust_auth_cidr_addresses'] = ['0.0.0.0/0','0.0.0.0/0'];
+              #     postgresql['md5_auth_cidr_addresses'] = ['0.0.0.0/0'];
+              #     postgresql['max_replication_slots'] = 1;
+              #     gitlab_rails['db_key_base'] = '4dd58204865eb41bca93bd38131d51cc';
+              #   OMNIBUS
+              #
+              #   primary.instance do
+              #     Component::Gitlab.perform do |secondary|
+              #       secondary.release = release
+              #       secondary.network = 'geo'
+              #       secondary.omnibus_config = <<~OMNIBUS
+              #         geo_secondary_role['enable'] = true;
+              #         gitlab_rails['db_key_base'] = '4dd58204865eb41bca93bd38131d51cc';
+              #       OMNIBUS
+              #
+              #       secondary.instance do
+              #         # shellout to instance specs
+              #       end
+              #     end
+              #   end
+              # end
             end
           end
         end
