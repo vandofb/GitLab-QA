ef76c88eafbad307eda4c05247e45cc5f4827734
Dylan Griffith <dyl.griffith@gmail.com>
2018-07-03 13:58:39 +0200

    Avoid creating tmp dir when we have CI_PROJECT_DIR

--- lib/gitlab/qa/component/internet_tunnel.rb
+++ lib/gitlab/qa/component/internet_tunnel.rb
@@ -16,7 +16,7 @@ module Gitlab
           @docker = Docker::Engine.new
           @volumes = {}
 
-          key_dir = ENV.fetch('CI_PROJECT_DIR', Dir.tmpdir)
+          key_dir = ENV['CI_PROJECT_DIR'] || Dir.tmpdir
           @ssh_key = Tempfile.new('tunnel-ssh-private-key', key_dir)
           @ssh_key.write(ENV.fetch('TUNNEL_SSH_PRIVATE_KEY'))
           @ssh_key.close
