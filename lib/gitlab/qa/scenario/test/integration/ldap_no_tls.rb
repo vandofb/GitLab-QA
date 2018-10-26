c7f30dd15a09a0f5a5cc0686e5e6d76a11e12878
Sanad Liaquat <sliaquat@gitlab.com>
2018-10-26 21:54:42 +0500

    Add missing file

--- lib/gitlab/qa/scenario/test/integration/ldap_no_tls.rb
+++ lib/gitlab/qa/scenario/test/integration/ldap_no_tls.rb
@@ -5,45 +5,19 @@ module Gitlab
     module Scenario
       module Test
         module Integration
-          class LDAP < Scenario::Template
-            attr_reader :gitlab_name, :spec_suite, :tls
+          class LDAPNoTLS < LDAP
+            def initialize
+              @gitlab_name = 'gitlab-ldap'
+              @spec_suite = 'Test::Integration::LDAPNoTLS'
+              @tls = false
+            end
 
             def configure(gitlab, ldap)
-              raise NotImplementedError
+              gitlab.omnibus_config = <<~OMNIBUS
+                    gitlab_rails['ldap_enabled'] = true;
+                    gitlab_rails['ldap_servers'] = #{ldap.to_config};
+              OMNIBUS
             end
-
-            # rubocop:disable Metrics/AbcSize
-            def perform(release)
-              Component::Gitlab.perform do |gitlab|
-                gitlab.release = release
-                gitlab.name = gitlab_name
-                gitlab.network = 'test'
-                gitlab.tls = tls
-
-                Component::LDAP.perform do |ldap|
-                  ldap.name = 'ldap-server'
-                  ldap.network = 'test'
-                  ldap.set_gitlab_credentials
-                  ldap.tls = tls
-
-                  configure(gitlab, ldap)
-
-                  ldap.instance do
-                    gitlab.instance do
-                      puts "Running #{spec_suite} specs!"
-
-                      Component::Specs.perform do |specs|
-                        specs.suite = spec_suite
-                        specs.release = gitlab.release
-                        specs.network = gitlab.network
-                        specs.args = [gitlab.address]
-                      end
-                    end
-                  end
-                end
-              end
-            end
-            # rubocop:enable Metrics/AbcSize
           end
         end
       end
