ef33b0a3f41c5d6a60b5d5974d888af3d403513b
Sanad Liaquat <sliaquat@gitlab.com>
2018-11-06 11:42:43 +0500

    Re added cron job setting

--- lib/gitlab/qa/scenario/test/integration/ldap_no_tls.rb
+++ lib/gitlab/qa/scenario/test/integration/ldap_no_tls.rb
@@ -15,7 +15,9 @@ module Gitlab
             def configure(gitlab, ldap)
               gitlab.omnibus_config = <<~OMNIBUS
                     gitlab_rails['ldap_enabled'] = true;
-                    gitlab_rails['ldap_servers'] = #{ldap.to_config};
+                    gitlab_rails['ldap_servers'] = #{ldap.to_config}; 
+                    gitlab_rails['ldap_sync_worker_cron'] = '* * * * *';
+                    gitlab_rails['ldap_group_sync_worker_cron'] = '* * * * *';
               OMNIBUS
             end
           end
