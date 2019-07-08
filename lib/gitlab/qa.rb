773800ee198eaf17fc904453f6cb68b2043c3ebd
Mark Lapierre <mlapierre@gitlab.com>
2019-07-08 00:15:42 +0000

    Add --parallel option
    Also rearrange options alphabetically
    
    Leave the LOAD_PATH alone. Specify relative paths instead.
    This avoid problems resolving constants when gitab-qa is
    required from the Gitlab project.

--- lib/gitlab/qa.rb
+++ lib/gitlab/qa.rb
@@ -1,74 +1,72 @@
-$LOAD_PATH << File.expand_path(__dir__)
-
 module Gitlab
   module QA
-    autoload :Release, 'qa/release'
-    autoload :Runner, 'qa/runner'
+    autoload :Release, 'gitlab/qa/release'
+    autoload :Runner, 'gitlab/qa/runner'
 
     module Runtime
-      autoload :Env, 'qa/runtime/env'
+      autoload :Env, 'gitlab/qa/runtime/env'
     end
 
     module Scenario
-      autoload :Actable, 'qa/scenario/actable'
-      autoload :Template, 'qa/scenario/template'
+      autoload :Actable, 'gitlab/qa/scenario/actable'
+      autoload :Template, 'gitlab/qa/scenario/template'
 
       module Test
         module Instance
-          autoload :Any, 'qa/scenario/test/instance/any'
-          autoload :Image, 'qa/scenario/test/instance/image'
-          autoload :RelativeUrl, 'qa/scenario/test/instance/relative_url'
-          autoload :Staging, 'qa/scenario/test/instance/staging'
-          autoload :Production, 'qa/scenario/test/instance/production'
-          autoload :Smoke, 'qa/scenario/test/instance/smoke'
-          autoload :Preprod, 'qa/scenario/test/instance/preprod'
+          autoload :Any, 'gitlab/qa/scenario/test/instance/any'
+          autoload :Image, 'gitlab/qa/scenario/test/instance/image'
+          autoload :RelativeUrl, 'gitlab/qa/scenario/test/instance/relative_url'
+          autoload :Staging, 'gitlab/qa/scenario/test/instance/staging'
+          autoload :Production, 'gitlab/qa/scenario/test/instance/production'
+          autoload :Smoke, 'gitlab/qa/scenario/test/instance/smoke'
+          autoload :Preprod, 'gitlab/qa/scenario/test/instance/preprod'
         end
 
         module Omnibus
-          autoload :Image, 'qa/scenario/test/omnibus/image'
-          autoload :Update, 'qa/scenario/test/omnibus/update'
-          autoload :Upgrade, 'qa/scenario/test/omnibus/upgrade'
+          autoload :Image, 'gitlab/qa/scenario/test/omnibus/image'
+          autoload :Update, 'gitlab/qa/scenario/test/omnibus/update'
+          autoload :Upgrade, 'gitlab/qa/scenario/test/omnibus/upgrade'
         end
 
         module Integration
-          autoload :Geo, 'qa/scenario/test/integration/geo'
-          autoload :LDAP, 'qa/scenario/test/integration/ldap'
-          autoload :LDAPNoTLS, 'qa/scenario/test/integration/ldap_no_tls'
-          autoload :LDAPTLS, 'qa/scenario/test/integration/ldap_tls'
-          autoload :SAML, 'qa/scenario/test/integration/saml'
-          autoload :GroupSAML, 'qa/scenario/test/integration/group_saml'
-          autoload :InstanceSAML, 'qa/scenario/test/integration/instance_saml'
-          autoload :Mattermost, 'qa/scenario/test/integration/mattermost'
-          autoload :Kubernetes, 'qa/scenario/test/integration/kubernetes'
-          autoload :ObjectStorage, 'qa/scenario/test/integration/object_storage'
-          autoload :OAuth, 'qa/scenario/test/integration/oauth'
-          autoload :Elasticsearch, 'qa/scenario/test/integration/elasticsearch'
+          autoload :Geo, 'gitlab/qa/scenario/test/integration/geo'
+          autoload :LDAP, 'gitlab/qa/scenario/test/integration/ldap'
+          autoload :LDAPNoTLS, 'gitlab/qa/scenario/test/integration/ldap_no_tls'
+          autoload :LDAPTLS, 'gitlab/qa/scenario/test/integration/ldap_tls'
+          autoload :SAML, 'gitlab/qa/scenario/test/integration/saml'
+          autoload :GroupSAML, 'gitlab/qa/scenario/test/integration/group_saml'
+          autoload :InstanceSAML, 'gitlab/qa/scenario/test/integration/instance_saml'
+          autoload :Mattermost, 'gitlab/qa/scenario/test/integration/mattermost'
+          autoload :Kubernetes, 'gitlab/qa/scenario/test/integration/kubernetes'
+          autoload :ObjectStorage, 'gitlab/qa/scenario/test/integration/object_storage'
+          autoload :OAuth, 'gitlab/qa/scenario/test/integration/oauth'
+          autoload :Elasticsearch, 'gitlab/qa/scenario/test/integration/elasticsearch'
         end
 
         module Sanity
-          autoload :Version, 'qa/scenario/test/sanity/version'
+          autoload :Version, 'gitlab/qa/scenario/test/sanity/version'
         end
       end
     end
 
     module Component
-      autoload :Gitlab, 'qa/component/gitlab'
-      autoload :InternetTunnel, 'qa/component/internet_tunnel'
-      autoload :LDAP, 'qa/component/ldap'
-      autoload :SAML, 'qa/component/saml'
-      autoload :Specs, 'qa/component/specs'
-      autoload :Staging, 'qa/component/staging'
-      autoload :Production, 'qa/component/production'
-      autoload :Minio, 'qa/component/minio'
-      autoload :Preprod, 'qa/component/preprod'
-      autoload :Elasticsearch, 'qa/component/elasticsearch'
+      autoload :Gitlab, 'gitlab/qa/component/gitlab'
+      autoload :InternetTunnel, 'gitlab/qa/component/internet_tunnel'
+      autoload :LDAP, 'gitlab/qa/component/ldap'
+      autoload :SAML, 'gitlab/qa/component/saml'
+      autoload :Specs, 'gitlab/qa/component/specs'
+      autoload :Staging, 'gitlab/qa/component/staging'
+      autoload :Production, 'gitlab/qa/component/production'
+      autoload :Minio, 'gitlab/qa/component/minio'
+      autoload :Preprod, 'gitlab/qa/component/preprod'
+      autoload :Elasticsearch, 'gitlab/qa/component/elasticsearch'
     end
 
     module Docker
-      autoload :Command, 'qa/docker/command'
-      autoload :Engine, 'qa/docker/engine'
-      autoload :Shellout, 'qa/docker/shellout'
-      autoload :Volumes, 'qa/docker/volumes'
+      autoload :Command, 'gitlab/qa/docker/command'
+      autoload :Engine, 'gitlab/qa/docker/engine'
+      autoload :Shellout, 'gitlab/qa/docker/shellout'
+      autoload :Volumes, 'gitlab/qa/docker/volumes'
     end
   end
 end
